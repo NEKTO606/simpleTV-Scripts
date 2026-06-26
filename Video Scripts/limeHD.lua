13bcf2eebd09c1405d3363bb5265585c1b29ffda
NEKTO606 <69595289+NEKTO606@users.noreply.github.com>
2026-06-26 13:27:53 +0300

    Add files via upload

--- Video Scripts/limeHD.lua
+++ Video Scripts/limeHD.lua
@@ -1,62 +1,46 @@
--- видеоскрипт для плейлиста "LimeHD", "LimeHD+" https://limehd.tv (15/3/26)
--- Copyright © 2017-2024 Nexterr | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для плейлиста "Lime HD" https://limehd.tv (26/6/26)
+-- Copyright © 2017-2026 Nexterr, NEKTO666 | https://github.com/NEKTO606/simpleTV-Scripts
 -- ## необходим ##
--- скрапер TVS: LimeHD_pls.lua, LimeHD+_pls.lua
--- расширение дополнения httptimeshift: limehd-timeshift_ext.lua
+-- скрапер TVS: lime_hd_pls.lua
 -- ## открывает подобные ссылки ##
--- https://limehd.tv/1
--- https://limehd.tv/channel/157
+-- https://limehd.tv/129
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
 		if not m_simpleTV.Control.CurrentAddress:match('^https?://limehd%.tv/') then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
 	local inAdr = m_simpleTV.Control.CurrentAddress
-	if not m_simpleTV.User then
-		m_simpleTV.User = {}
-	end
-	if not m_simpleTV.User.limehd then
-		m_simpleTV.User.limehd = {}
-	end
+	inAdr = inAdr:gsub('$OPT:.+', '')
+	local id = tonumber(inAdr:match('([^/]%d*)$'))
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local userAgent = 'Mozilla/5.0 (iPad; CPU OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Mobile/15E148 Safari/605.1.15'
-	local session = m_simpleTV.Http.New(userAgent)
+	local session = m_simpleTV.Http.New('LimeHDTV/5.0.0 (com.infolink.LimeHDTV; build:1; iOS 16.2.0) Alamofire/5.0.0')
+		if not session then return end
+	m_simpleTV.Http.SetTimeout(session, 12000)
+	local header = decode64('WC1MSEQtQWdlbnQ6IHsidmVyc2lvbl9uYW1lIjoiNS4wLjAiLCJ2ZXJzaW9uX2NvZGUiOiI1MDAwMCIsInBsYXRmb3JtIjoiaW9zIiwibmFtZSI6ImlQaG9uZSIsImRldmljZV9pZCI6IjE0MzJGNzhFLUY4NzctNEY3OS1BRUE4LUQzRUJBRDBBQTBCMyIsImFwcCI6ImNvbS5pbmZvbGluay5MaW1lSERUViJ9')
+	local rc, answer = m_simpleTV.Http.Request(session, {url = decode64('aHR0cHM6Ly9wbC5pcHR2MjAyMS5jb20vYXBpL3YxL3BsYXlsaXN0'), headers = header})
+		if rc ~= 200 or not answer then return end
+	answer = answer:gsub('\\', '\\\\')
+	answer = answer:gsub('\\"', '\\\\"')
+	answer = answer:gsub('\\/', '/')
+	answer = answer:gsub('%[%]', '""')
+	require 'json'
+	local err, tab = pcall(json.decode, answer)
+		if not tab then return end
+	local retAdr
+	for i = 1, #tab.channels do
+		if tab.channels[i].id == id then
+			retAdr = tab.channels[i].url
+		 break
+		end
+	end
+	m_simpleTV.Http.Close(session)
+		if not retAdr then return end
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 8000)
-	local function getStream(inAdr)
-		local id = inAdr:match('%d+')
-			if not id then return end
-		local url = decode64('aHR0cHM6Ly9hcGkuaXB0djIwMjEuY29tL3YxL3N0cmVhbXMv') .. id
-		-- aHR0cHM6Ly9hcGkuaXB0djIwMjEuY29tL3YxL2NoYW5uZWxz
-		local headers = decode64('WC1BY2Nlc3MtS2V5OiAxMGFhMDkxMTQ1ODhhNWY3NTBlYWVkNWU5ZGU1MzcwNGM4NThlMTQ0')
-		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
-			if rc ~= 200 then return end
-	 return answer:match('"playlist_url":"([^"]+)'), answer:match('"archive_url":"([^"]+)'), answer:match('"archive_hours":(%d+)')
-	end
-	local function getStreamFromApp(inAdr)
-		local id = inAdr:match('%d+')
-			if not id then return end
-		local url = decode64('aHR0cHM6Ly9wbC5pcHR2MjAyMS5jb20vYXBpL3Y0L2NoYW5uZWw/aWQ9') .. id .. '&tz=3&region=0'
-		local headers = decode64('WC1MSEQtQWdlbnQ6IHsidmVyc2lvbl9uYW1lIjoiMS4wLjIuMjAzIiwidmVyc2lvbl9jb2RlIjoiMjAzIiwicGxhdGZvcm0iOiJ3aW4iLCJkZXZpY2VfaWQiOiIgICAgIDAwMDAwMDAwIiwiYXBwIjoidHYubGltZWhkLndpbiIsImdlbmVyYXRpb24iOiIyIn0')
-		local rc, answer = m_simpleTV.Http.Request(session, {url = url, headers = headers})
-			if rc ~= 200 then return end
-	 return answer:match('"common":"([^"]+)'), answer:match('"archive":"([^"]+)')
-	end
-	local retAdr, url_archive, archive_hours
-	if inAdr:match('/channel/') then
-		retAdr, url_archive = getStreamFromApp(inAdr)
-		retAdr = retAdr:gsub('/mhd.', '/cdn-lb.')
-	else
-		retAdr, url_archive, archive_hours = getStream(inAdr)
-	end
-		if not retAdr then return end
-	if url_archive then
-		url_archive = url_archive:gsub('/$', '')
-	end
-	m_simpleTV.User.limehd.url_archive = url_archive
-	local extOpt = '$OPT:adaptive-minbuffer=30000$OPT:http-user-agent=' .. userAgent
 	local rc, answer = m_simpleTV.Http.Request(session, {url = retAdr})
+	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	local t = {}
 		for w in answer:gmatch('EXT%-X%-STREAM%-INF(.-)\n') do
@@ -68,11 +52,11 @@
 				t[#t + 1] = {}
 				t[#t].Id = tonumber(res)
 				t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
-				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', retAdr, bw, extOpt)
+				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s', retAdr, bw)
 			end
 		end
 		if #t == 0 then
-			m_simpleTV.Control.CurrentAddress = retAdr .. extOpt
+			m_simpleTV.Control.CurrentAddress = retAdr
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
@@ -84,7 +68,7 @@
 	t[#t + 1] = {}
 	t[#t].Id = 100000
 	t[#t].Name = '▫ адаптивное'
-	t[#t].Address = retAdr .. extOpt
+	t[#t].Address = retAdr
 	local index = #t
 		for i = 1, #t do
 			if t[i].Id >= lastQuality then
@@ -107,11 +91,3 @@
 		m_simpleTV.Config.SetValue('limehd_qlty', id)
 	end
 -- debug_in_file(t[index].Address .. '\n')
--- catchup m3u -------------------------------------------------------------
--- local info = m_simpleTV.Control.GetCurrentChannelInfo()
-	-- if not info or info.Id == - 1 then return end
--- if archive_hours and archive_hours == '0' and url_archive then
-	-- archive_hours = 120
--- end
--- debug_in_file('#EXTINF:-1 catchup="append" catchup-minutes="' .. ((archive_hours or 0) * 60) .. '",' .. info.Title ..'\n' .. inAdr.. '\n')
-----------------------------------------------------------------------------------
