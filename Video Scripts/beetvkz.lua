8d0030ef10be5f3c61920d1c31c5ca7c507a289a
NEKTO606 <69595289+NEKTO606@users.noreply.github.com>
2026-07-01 10:27:32 +0300

    Add files via upload

--- Video Scripts/beetvkz.lua
+++ Video Scripts/beetvkz.lua
@@ -1,5 +1,5 @@
--- видеоскрипт для плейлиста "BeeTV KZ" https://beetv.kz (26/6/26)
--- Copyright © 2017-2026 Nexterr, NEKTO666 | https://github.com/Nexterr-origin/simpleTV-Scripts
+-- видеоскрипт для плейлиста "BeeTV KZ" https://beetv.kz (1/7/26)
+-- Copyright © 2017-2026 Nexterr, NEKTO666 | https://github.com/NEKTO606/simpleTV-Scripts
 -- ## необходим ##
 -- скрапер TVS: beetvkz_pls.lua
 -- ## открывает подобные ссылки ##
@@ -12,24 +12,19 @@
 	local inAdr = m_simpleTV.Control.CurrentAddress
 	inAdr = inAdr:gsub('$OPT:.+', '')
 	local id = inAdr:match('([^/]%d+)$')
-	local prx = ''
-	inAdr = string.format(decode64('aHR0cHM6Ly8xNzYuMjIyLjE5MS41Ny9icGstdHYvJXMvdHZlL2luZGV4Lm1wZA'), id)
+	inAdr = string.format(decode64('aHR0cHM6Ly9zcG92dC5ydS9icGstdHYvJXMvdHZlL2luZGV4Lm0zdTg'), id)
 	m_simpleTV.Control.ChangeAddress = 'Yes'
 	m_simpleTV.Control.CurrentAddress = 'error'
-	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0', prx, true)
+	local session = m_simpleTV.Http.New('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0')
 		if not session then return end
 	m_simpleTV.Http.SetTimeout(session, 12000)
-	m_simpleTV.Http.SetRedirectAllow(session, false)
-	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr})
-		if rc ~= 307 then return end
-	local head = m_simpleTV.Http.GetRawHeader(session)
-	local adr = head:match('Location:%s?([^\n]+)')
-		if not adr then return end
-	local rc, answer = m_simpleTV.Http.Request(session, {url = adr})
+	local extOpt = '$OPT:http-referrer=https://spovt.ru/'
+	local headers = 'Referer: https://spovt.ru/'
+	local rc, answer = m_simpleTV.Http.Request(session, {url = inAdr, headers = headers})
 		if rc ~= 200 then return end
 	m_simpleTV.Http.Close(session)
 	local gm, rs, bn
-	if adr:match('.m3u8') then
+	if inAdr:match('.m3u8') then
 		gm = 'EXT%-X%-STREAM%-INF.-\n'
 		rs = 'resolution=%d+x(%d+)'
 		bn = ':bandwidth=(%d+)'
@@ -49,11 +44,11 @@
 				t[#t + 1] = {}
 				t[#t].Id = bw
 				t[#t].Name = res .. 'p (' .. bw .. ' кбит/с)'
-				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s', adr, bw)
+				t[#t].Address = string.format('%s$OPT:adaptive-logic=highest$OPT:adaptive-max-bw=%s%s', inAdr, bw, extOpt)
 			end
 		end
 		if #t == 0 then
-			m_simpleTV.Control.CurrentAddress = adr
+			m_simpleTV.Control.CurrentAddress = inAdr .. extOpt
 		 return
 		end
 	table.sort(t, function(a, b) return a.Id < b.Id end)
@@ -65,7 +60,7 @@
 	t[#t + 1] = {}
 	t[#t].Id = 50000
 	t[#t].Name = '▫ адаптивное'
-	t[#t].Address = adr
+	t[#t].Address = inAdr .. extOpt
 	local index = #t
 		for i = 1, #t do
 			if t[i].Id >= lastQuality then
